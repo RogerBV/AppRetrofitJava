b444e47aa2e4fae43594c2eb54927be9639957be
RogerBV <rogerbv_9017@hotmail.com>
2019-06-24 15:01:29 -0500

    Work with Retrofit

--- app/src/main/java/com/prueba/roger/appretrofitjava/MainActivity.java
+++ app/src/main/java/com/prueba/roger/appretrofitjava/MainActivity.java
@@ -1,5 +1,6 @@
 package com.prueba.roger.appretrofitjava;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -11,13 +12,15 @@ import com.google.gson.GsonBuilder;
 import com.prueba.roger.appretrofitjava.data.api.RestClient;
 import com.prueba.roger.appretrofitjava.data.model.PokemonFeed;
 
+import java.util.List;
+
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements MainView {
 
     private RecyclerView recyclerView;
     private MainPresenter presenter;
@@ -27,52 +30,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        initViews();
-    }
-    private void initViews(){
         recyclerView = (RecyclerView)findViewById(R.id.recycler);
-        recyclerView.setHasFixedSize(true);
-        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
-        recyclerView.setLayoutManager(layoutManager);
-        loadJSON();
-    }
-    private void loadJSON(){
-        Gson gson = new GsonBuilder()
-                .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
-                .create();
-
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("http://pokeapi.co/api/v2/")
-                .addConverterFactory(GsonConverterFactory.create(gson))
-                .build();
-
-        RestClient restClient = retrofit.create(RestClient.class);
-        Call<PokemonFeed> call = restClient.getData();
-
-        call.enqueue(new Callback<PokemonFeed>() {
-            @Override
-            public void onResponse(Call<PokemonFeed> call, Response<PokemonFeed> response) {
-                switch (response.code()) {
-                    case 200:
-                        PokemonFeed data = response.body();
-                        view.notifyDataSetChanged(data.getResults());
-                        break;
-                    case 401:
-
-                        break;
-                    default:
-
-                        break;
-                }
-            }
-
-            @Override
-            public void onFailure(Call<PokemonFeed> call, Throwable t) {
-                //Log.e("error", t.toString());
-            }
-        });
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        presenter = new MainPresenter(this);
+        presenter.getData();
     }
 
+    @Override
+    public void notifyDataSetChanged(List<Result> pokemon) {
+        adapter.swap(pokemon);
+    }
 
-
+    @Override
+    public void navigateToDetails(int idPokemon) {
+        startActivity(new Intent(this,PokemonDetailActivity.class).putExtra("idpokemon",idPokemon));
+    }
 }
